1bcdb68b54e92126b2f13d8b806e50741f3f5a49
Michael Burke <michaelburke@michaels-macbook-pro-2.local>
2018-09-11 18:00:46 -0400

    eight ball

--- magicEightBall/ViewController.swift
+++ magicEightBall/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     @IBOutlet weak var resultLabel: UILabel!
+    
+     let answerArray = ["It is certain",
+                       "It is decidedly so",
+                       "Without a doubt",
+                       "Yes, definitely",
+                       "You may rely on it",
+                       "As I see it, yes",
+                       "Most likely",
+                       "Outlook good",
+                       "Yes",
+                       "Signs point to yes",
+                       "Reply hazy try again",
+                       "Ask again later",
+                       "Better not tell you now",
+                       "Cannot predict now",
+                       "Concentrate and ask again",
+                       "Don't count on it",
+                       "My reply is no",
+                       "My sources say no",
+                       "Outlook not so good",
+                       "Very doubtful"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        resultLabel.text = answerArray.randomElement()!
+    }
+    
 }
 
